89065e64638ecf5dc5e17e1676ba944cfc8a7a26
Edilson Matola <edilsonmatola13@gmail.com>
2022-04-05 14:06:09 +0200

    Changes in the System UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'screens/sign_in_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(
+      statusBarColor: Colors.white,
+      statusBarIconBrightness: Brightness.dark,
+    ),
+  );
   await Firebase.initializeApp();
   runApp(const MyApp());
 }
