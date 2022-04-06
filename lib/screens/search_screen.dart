d8277dde84170575e7e03c319a9d659a065180f6
Edilson Matola <edilsonmatola13@gmail.com>
2022-04-06 09:46:44 +0200

    Add search Functionality to show users

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 import '../models/user_model.dart';
@@ -13,6 +14,57 @@ class SearchScreen extends StatefulWidget {
 
 class _SearchScreenState extends State<SearchScreen> {
   final TextEditingController searchController = TextEditingController();
+
+  List<Map> searchResult = [];
+
+  bool isLoading = false;
+
+  Future<void> onSearch() async {
+    setState(() {
+      searchResult = [];
+      isLoading = true;
+    });
+    await FirebaseFirestore.instance
+        .collection('users')
+        .where(
+          'name',
+          isEqualTo: searchController.text,
+        )
+        .get()
+        .then(
+      (value) {
+        if (value.docs.isEmpty) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              duration: Duration(
+                seconds: 3,
+              ),
+              elevation: 2,
+              content: Center(
+                child: Text(
+                  'No users found',
+                ),
+              ),
+            ),
+          );
+          setState(() {
+            isLoading = false;
+          });
+          return;
+        }
+        for (final user in value.docs) {
+          if (user.data()['email'] != widget.user.email) {
+            searchResult.add(
+              user.data(),
+            );
+          }
+        }
+        setState(() {
+          isLoading = false;
+        });
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,17 +89,52 @@ class _SearchScreenState extends State<SearchScreen> {
                           borderSide: BorderSide.none,
                         ),
                         hintText: 'Type username',
-                        prefixIcon: const Icon(
-                          Icons.search,
-                          color: Colors.grey,
-                        ),
                         filled: true,
+                        suffixIcon: IconButton(
+                          onPressed: onSearch,
+                          icon: const Icon(
+                            Icons.search_outlined,
+                          ),
+                        ),
                       ),
                     ),
                   ),
                 ),
               ],
-            )
+            ),
+            if (searchResult.isNotEmpty)
+              Expanded(
+                child: ListView.builder(
+                  itemCount: searchResult.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      leading: CircleAvatar(
+                        child: Image.network(
+                          searchResult[index]['image'] as String,
+                        ),
+                      ),
+                      title: Text(
+                        searchResult[index]['name'] as String,
+                      ),
+                      subtitle: Text(
+                        searchResult[index]['email'] as String,
+                      ),
+                      trailing: IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.message_outlined,
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              )
+            else if (isLoading == true)
+              Center(
+                child: CircularProgressIndicator(
+                  color: Colors.white,
+                ),
+              )
           ],
         ),
       ),
