aac3e31900289fd6e35bd173219eb233b137b2a2
Edilson Matola <edilsonmatola13@gmail.com>
2022-04-07 09:00:56 +0200

    Fetching messages from Firebase

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,6 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 import '../models/user_model.dart';
+import '../widgets/message_bubble.dart';
 import '../widgets/message_textfield.dart';
 
 class ChatScreen extends StatelessWidget {
@@ -43,28 +45,70 @@ class ChatScreen extends StatelessWidget {
           ],
         ),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Expanded(
-              child: Container(
-                padding: const EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(25),
-                    topRight: Radius.circular(25),
-                  ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(25),
+                  topRight: Radius.circular(25),
                 ),
-                child: Container(),
+              ),
+              child: StreamBuilder(
+                stream: FirebaseFirestore.instance
+                    .collection('users')
+                    .doc(currentUser.uid)
+                    .collection('messages')
+                    .doc(friendId)
+                    .collection('chats')
+                    .orderBy(
+                      'dateTime',
+                      descending: true,
+                    )
+                    .snapshots(),
+                builder: (context,
+                    AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>>
+                        snapshot) {
+                  if (snapshot.hasData) {
+                    if (snapshot.data!.docs.isEmpty) {
+                      return Center(
+                        child: Text(
+                          'Say Hi',
+                        ),
+                      );
+                    }
+                    return ListView.builder(
+                      physics: const BouncingScrollPhysics(),
+                      reverse: true,
+                      itemCount: snapshot.data!.docs.length,
+                      itemBuilder: (context, index) {
+                        final isMe = snapshot.data!.docs[index]['senderId'] ==
+                            currentUser.uid;
+                        return MessageBubble(
+                          message:
+                              snapshot.data!.docs[index]['message'] as String,
+                          isMe: isMe,
+                        );
+                      },
+                    );
+                  }
+                  return Center(
+                    child: CircularProgressIndicator(
+                      color: Colors.white,
+                    ),
+                  );
+                },
               ),
             ),
-            MessageTextField(
-              currentId: currentUser.uid,
-              friendId: friendId,
-            ),
-          ],
-        ),
+          ),
+          MessageTextField(
+            currentId: currentUser.uid,
+            friendId: friendId,
+          ),
+        ],
       ),
     );
   }
