83d931fa4409243ea80f0b98c7d28357308b012d
Edilson Matola <edilsonmatola13@gmail.com>
2022-04-07 09:03:10 +0200

    Create Message bubble

---
+++ lib/widgets/message_bubble.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class MessageBubble extends StatelessWidget {
+  MessageBubble({Key? key, required this.message, required this.isMe})
+      : super(key: key);
+
+  final String message;
+  final bool isMe;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
+      children: [
+        Container(
+          padding: const EdgeInsets.all(16),
+          margin: const EdgeInsets.all(16),
+          constraints: BoxConstraints(
+            maxWidth: 200,
+          ),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12),
+            color: isMe ? Colors.teal : Colors.grey[600],
+          ),
+          child: Text(
+            message,
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
