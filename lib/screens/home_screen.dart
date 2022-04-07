e1307adb979e0bf95bccc71193ade7a7c6d0448d
Edilson Matola <edilsonmatola13@gmail.com>
2022-04-07 14:05:06 +0200

    Add Alert Dialog Confirmation

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -23,114 +23,113 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        centerTitle: true,
-        backgroundColor: Colors.teal,
+        backgroundColor: const Color.fromRGBO(36, 35, 49, 1),
         title: Text(
-          'Home',
+          'ChatApp',
         ),
         actions: [
           IconButton(
-            onPressed: () async {
-              await GoogleSignIn().signOut();
-              await FirebaseAuth.instance.signOut();
-              await Navigator.pushAndRemoveUntil(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => SignInScreen(),
-                ),
-                (route) => false,
-              );
-            },
+            onPressed: showSignOutConfirmation,
             icon: const Icon(
               Icons.logout_rounded,
             ),
           ),
         ],
       ),
-      body: StreamBuilder(
-        stream: FirebaseFirestore.instance
-            .collection('users')
-            .doc(widget.user.uid)
-            .collection('messages')
-            .snapshots(),
-        builder: (context,
-            AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
-          if (snapshot.hasData) {
-            if (snapshot.data!.docs.isEmpty) {
-              return const Center(
-                child: Text(
-                  'No Chats Available',
-                ),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 4),
+        child: StreamBuilder(
+          stream: FirebaseFirestore.instance
+              .collection('users')
+              .doc(widget.user.uid)
+              .collection('messages')
+              .snapshots(),
+          builder: (context,
+              AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+            if (snapshot.hasData) {
+              if (snapshot.data!.docs.isEmpty) {
+                return const Center(
+                  child: Text(
+                    'No Chats Available',
+                  ),
+                );
+              }
+              return ListView.builder(
+                itemCount: snapshot.data!.docs.length,
+                itemBuilder: (context, index) {
+                  final friendId = snapshot.data!.docs[index].id;
+                  final lastMessage =
+                      snapshot.data!.docs[index]['last_message'];
+                  return FutureBuilder(
+                    future: FirebaseFirestore.instance
+                        .collection('users')
+                        .doc(friendId)
+                        .get(),
+                    builder: (context, AsyncSnapshot asyncSnapshot) {
+                      if (asyncSnapshot.hasData) {
+                        final friend = asyncSnapshot.data;
+                        return ListTile(
+                          // trailing: Text(''),  //TODO: Display the hour
+                          onTap: () => Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => ChatScreen(
+                                currentUser: widget.user,
+                                friendId: friend['uid'] as String,
+                                friendName: friend['userName'] as String,
+                                friendImage: friend['image'] as String,
+                              ),
+                            ),
+                          ),
+                          leading: ClipRRect(
+                            borderRadius: BorderRadius.circular(80),
+                            child: CachedNetworkImage(
+                              height: 50,
+                              imageUrl: friend['image'] as String,
+                              placeholder: (context, url) =>
+                                  CircularProgressIndicator(
+                                color: Colors.white,
+                              ),
+                              errorWidget: (context, url, error) => const Icon(
+                                Icons.error_outline,
+                              ),
+                            ),
+                          ),
+                          title: Text(
+                            friend['userName'] as String,
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          subtitle: Text(
+                            '$lastMessage',
+                            style: TextStyle(
+                              color: Colors.grey[600],
+                              fontSize: 15,
+                              fontWeight: FontWeight.w500,
+                            ),
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        );
+                      }
+                      return LinearProgressIndicator(
+                        color: Colors.white,
+                      );
+                    },
+                  );
+                },
               );
             }
-            return ListView.builder(
-              itemCount: snapshot.data!.docs.length,
-              itemBuilder: (context, index) {
-                final friendId = snapshot.data!.docs[index].id;
-                final lastMessage = snapshot.data!.docs[index]['last_message'];
-                return FutureBuilder(
-                  future: FirebaseFirestore.instance
-                      .collection('users')
-                      .doc(friendId)
-                      .get(),
-                  builder: (context, AsyncSnapshot asyncSnapshot) {
-                    if (asyncSnapshot.hasData) {
-                      final friend = asyncSnapshot.data;
-                      return ListTile(
-                        onTap: () => Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => ChatScreen(
-                              currentUser: widget.user,
-                              friendId: friend['uid'] as String,
-                              friendName: friend['userName'] as String,
-                              friendImage: friend['image'] as String,
-                            ),
-                          ),
-                        ),
-                        leading: ClipRRect(
-                          borderRadius: BorderRadius.circular(80),
-                          child: CachedNetworkImage(
-                            height: 50,
-                            imageUrl: friend['image'] as String,
-                            placeholder: (context, url) =>
-                                CircularProgressIndicator(
-                              color: Colors.white,
-                            ),
-                            errorWidget: (context, url, error) => const Icon(
-                              Icons.error_outline,
-                            ),
-                          ),
-                        ),
-                        title: Text(
-                          friend['userName'] as String,
-                        ),
-                        subtitle: Text(
-                          '$lastMessage',
-                          style: TextStyle(
-                            color: Colors.grey,
-                          ),
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                      );
-                    }
-                    return LinearProgressIndicator(
-                      color: Colors.white,
-                    );
-                  },
-                );
-              },
+            return const Center(
+              child: CircularProgressIndicator(
+                color: Colors.white,
+              ),
             );
-          }
-          return const Center(
-            child: CircularProgressIndicator(
-              color: Colors.white,
-            ),
-          );
-        },
+          },
+        ),
       ),
       floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.teal,
+        backgroundColor: const Color.fromRGBO(36, 35, 49, 1),
         onPressed: () {
           Navigator.push(
             context,
@@ -142,8 +141,57 @@ class _HomeScreenState extends State<HomeScreen> {
           );
         },
         child: const Icon(
-          Icons.search_outlined,
+          Icons.message,
         ),
+      ),
+    );
+  }
+
+  void showSignOutConfirmation() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text(
+          'Sign out',
+        ),
+        content: Text(
+          'Are you sure you want to sign out?',
+        ),
+        actions: [
+          // * Cancel button
+          TextButton(
+            style: TextButton.styleFrom(
+              primary: Colors.black,
+            ),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: Text(
+              'Cancel',
+            ),
+          ),
+          // * Sign out
+          TextButton(
+            style: TextButton.styleFrom(
+              primary: Colors.red,
+            ),
+            onPressed: () async {
+              Navigator.of(context).pop();
+              await GoogleSignIn().signOut();
+              await FirebaseAuth.instance.signOut();
+              await Navigator.pushAndRemoveUntil(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => SignInScreen(),
+                ),
+                (route) => false,
+              );
+            },
+            child: Text(
+              'Sign out',
+            ),
+          ),
+        ],
       ),
     );
   }
